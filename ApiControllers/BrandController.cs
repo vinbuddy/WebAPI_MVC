8e16e3e3719a2bfca5c710d1006fa212f1aa9758
vinbuddy <94288269+vinbuddy@users.noreply.github.com>
2023-09-09 19:31:20 +0700

    Initial commit

---
+++ ApiControllers/BrandController.cs
@@ -0,0 +1,56 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI_MVC.Models;
+
+namespace WebAPI_MVC.ApiControllers
+{
+    public class BrandController : ApiController
+    {
+        public IEnumerable<Brand> Get()
+        {
+            StoreEntities db = new StoreEntities();
+            List<Brand> brands = db.Brands.ToList();
+
+            return brands;
+        }
+
+        public IEnumerable<Brand> Post(Brand brand)
+        {
+            StoreEntities db = new StoreEntities();
+            db.Brands.Add(brand);
+            db.SaveChanges();
+
+            // response brand list to post method
+            return db.Brands.ToList();
+        }
+
+        public IEnumerable<Brand> Put(Brand brand)
+        {
+            StoreEntities db = new StoreEntities();
+            Brand oldBrand = db.Brands.Where(br => br.BrandID == brand.BrandID).FirstOrDefault();
+
+            oldBrand.BrandName = brand.BrandName;
+
+            db.SaveChanges();
+
+            // Response
+            return db.Brands.ToList();
+        }
+
+        public IEnumerable<Brand> Delete(long id)
+        {
+            StoreEntities db = new StoreEntities();
+            Brand brand = db.Brands.Where(br => br.BrandID == id).FirstOrDefault();
+
+            db.Brands.Remove(brand);
+            db.SaveChanges();
+
+            // Response
+            return db.Brands.ToList();
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+
+namespace WebAPI_MVC
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Config JSON response
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+        }
+    }
+}
